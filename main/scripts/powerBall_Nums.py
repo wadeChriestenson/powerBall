c883adbd152dd40dfb9861f0cdb3ede2d64f0b9a
wadec <wadechriestenson@gmail.com>
2022-11-12 11:06:58 -0800

    WC - 11/12/22 Updated Graphs for all Picks added Median Numbers for all picks

--- main/scripts/powerBall_Nums.py
+++ main/scripts/powerBall_Nums.py
@@ -10,8 +10,7 @@ app = Dash(__name__)
 input_csv = pd.read_csv('../csv/Powerball_History.csv')
 
 powerBall_DF = pd.DataFrame(input_csv)
-# powerBall_DF = powerBall_DF.sort_values(by=['Date'], ascending=True)
-# print(powerBall_DF.columns)
+
 Date, \
 Num_1, \
 Num_2, \
@@ -44,7 +43,16 @@ print(f'{Median_Num_1:.0f}',
       f'{Median_Num_4:.0f}',
       f'{Median_Num_5:.0f}',
       f'{Median_Powerball:.0f}')
-print('Num_1 has a total of', len(Num_1), 'Draws')
+
+fig_median = px.scatter(x=[1, 2, 3, 4, 5, 6],
+                        y=[Median_Num_1,
+                           Median_Num_2,
+                           Median_Num_3,
+                           Median_Num_4,
+                           Median_Num_5,
+                           Median_Powerball
+                           ]
+                        )
 
 fig1 = px.scatter(x=Num_1, y=Date)
 fig2 = px.scatter(x=Num_2, y=Date)
@@ -54,18 +62,19 @@ fig5 = px.scatter(x=Num_5, y=Date)
 figPowerball = px.scatter(x=Powerball, y=Date)
 
 app.layout = html.Div(children=[
-    html.H1(children='History of All Powerball Picks'),
-
-    html.Div(children='''
-         All ''' + str(len(Date)) + ''' Picks for the Powerball from 02/03/2010 to 11/09/2022
-         The Median Numbers for ''' + f'{Median_Num_1:.0f}' + ''' ''' 
-                      f'{Median_Num_2:.0f}' + ''' '''
-                      f'{Median_Num_3:.0f}' + ''' '''
-                      f'{Median_Num_4:.0f}' + ''' '''
-                      f'{Median_Num_5:.0f}' + ''' '''
-                      f'{Median_Powerball:.0f}' + '''
-    '''),
-
+    html.H1(children='History of Powerball Picks'),
+    html.H2(children='''All ''' + str(len(Date)) + ''' Picks for the Powerball from 02/03/2010 to 11/09/2022'''),
+    html.H3(children='''The Median Numbers of All Picks: ''' +
+                     f'{Median_Num_1:.0f}' + ''' '''   
+                     f'{Median_Num_2:.0f}' + ''' '''
+                     f'{Median_Num_3:.0f}' + ''' '''
+                     f'{Median_Num_4:.0f}' + ''' '''
+                     f'{Median_Num_5:.0f}' + ''' '''
+                     f'{Median_Powerball:.0f}'''),
+    dcc.Graph(
+        id='median',
+        figure=fig_median
+    ),
     dcc.Graph(
         id='Num_1',
         figure=fig1
